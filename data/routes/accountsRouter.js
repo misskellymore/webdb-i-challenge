c120c3124f555451babdd155b2da331e792a408f
Kelly Moreira <moreira.kd@outlook.com>
2019-10-07 18:43:09 -0400

    post working

--- data/routes/accountsRouter.js
+++ data/routes/accountsRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const db = require('../dbConfig.js');
 
+// Get
 
 router.get('/', (req, res) => {
     db('accounts').select()
@@ -28,6 +29,24 @@ router.get('/:id', validateAccountID, (req, res) => {
     })
 });
 
+// Post
+
+router.post('/', validateAccount, (req,res) => {
+    const account = req.body;
+
+    db('accounts').insert(account)
+    .then(added => {
+        res.status(201).json(added[0]);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+
+
+// middlewares
+
 
 function validateAccountID (req, res, next) {
     const accountBody = req.body;
@@ -42,6 +61,20 @@ function validateAccountID (req, res, next) {
             res.status(404).json({message: 'Cannot find account with this ID'})
         }
     })
+}
+
+
+function validateAccount (req, res, next) {
+    const accountBody = req.body;
+
+    if(!Object.keys(accountBody).length) {
+        res.status(400).json({message: 'missing account fields'})
+    } else if (!accountBody.name || !accountBody.budget) {
+        res.status(400).json({message: 'missing name and budget fields'})
+
+    } else {
+        next();
+    }
 }
 
 module.exports = router;
